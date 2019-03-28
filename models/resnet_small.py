bec835f0c597852e8034315d612554eb8defdef2
zhangfeihu <fykalviny@gmail.com>
2019-03-28 14:02:12 +0800

    clear

--- models/resnet_small.py
+++
@@ -1,7 +0,0 @@
-from torchvision.models.resnet import resnet50
-
-
-def createModel(depth, data, num_classes, death_mode='none', death_rate=0.5,
-                **kwargs):
-    print('Create ResNet-50 for {}'.format(data))
-    return resnet50()
